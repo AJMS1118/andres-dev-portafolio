9698490a7e8fd41b14dd5d57922552c239984941
AJMS1118 <ajquesadac90@gmail.com>
2023-11-19 21:47:25 -0500

    making the app skeleton, and installing some missing dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
-import { Navbar } from "./components";
+import { BrowserRouter } from "react-router-dom";
+import {
+	Navbar,
+	Experience,
+	Feedbacks,
+	Contact,
+	Hero,
+	About,
+	Works,
+	StarsCanvas,
+	Tech,
+} from "./components";
 
 function App() {
 	return (
 		<>
-			<div>Mi portafolio 3d!!!</div>
-			<Navbar />
+			<BrowserRouter>
+				<div className="relative z-0 bg-primary">
+					<div className="bg-hero-patters bg-cover bg-center bg-no-repeat">
+						<Navbar />
+						<Hero />
+					</div>
+					<About />
+					<Experience />
+					<Tech />
+					<Works />
+					<Feedbacks />
+					<div className="relative z-0">
+						<Contact />
+						<StarsCanvas />
+					</div>
+				</div>
+			</BrowserRouter>
 		</>
 	);
 }
